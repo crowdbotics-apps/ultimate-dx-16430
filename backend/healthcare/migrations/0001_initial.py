2555999ffdf5505c741235e99962e89d7e5a700d
Crowdbotics <team@crowdbotics.com>
2020-04-30 05:26:23 +0000

    Update models

--- backend/healthcare/migrations/0001_initial.py
+++ backend/healthcare/migrations/0001_initial.py
@@ -8,36 +8,73 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Step',
+            name="Step",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('order', models.IntegerField()),
-                ('title', models.CharField(max_length=256)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("order", models.IntegerField()),
+                ("title", models.CharField(max_length=256)),
             ],
         ),
         migrations.CreateModel(
-            name='Question',
+            name="Question",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('type', models.CharField(max_length=256)),
-                ('data_type', models.CharField(max_length=256)),
-                ('order', models.IntegerField()),
-                ('title', models.CharField(blank=True, max_length=1000, null=True)),
-                ('step', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='question_step', to='healthcare.Step')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("type", models.CharField(max_length=256)),
+                ("data_type", models.CharField(max_length=256)),
+                ("order", models.IntegerField()),
+                ("title", models.CharField(blank=True, max_length=1000, null=True)),
+                (
+                    "step",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="question_step",
+                        to="healthcare.Step",
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Choice',
+            name="Choice",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=256)),
-                ('value', models.CharField(max_length=256)),
-                ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='choice_question', to='healthcare.Question')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=256)),
+                ("value", models.CharField(max_length=256)),
+                (
+                    "question",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="choice_question",
+                        to="healthcare.Question",
+                    ),
+                ),
             ],
         ),
     ]
