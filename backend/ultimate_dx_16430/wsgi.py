c977b36e676444ccb76a63b2d55a84e7c20e04a0
Crowdbotics <team@crowdbotics.com>
2020-04-30 04:58:26 +0000

    Update models

--- backend/ultimate_dx_16430/wsgi.py
+++ backend/ultimate_dx_16430/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ultimate_dx_16430.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ultimate_dx_16430.settings")
 
 application = get_wsgi_application()
